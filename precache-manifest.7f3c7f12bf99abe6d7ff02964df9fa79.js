47708d9760b4a8509c7db27c6e68790a7c8b3adc
SHUBHAM KUMAR <shubhamkmr06082001@gmail.com>
2020-07-19 07:35:12 +0530

    Updates

---
+++ precache-manifest.7f3c7f12bf99abe6d7ff02964df9fa79.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59516242386f262351242f380486e8fd",
+    "url": "/KEEPER-APP/index.html"
+  },
+  {
+    "revision": "c062f163a0f854126674",
+    "url": "/KEEPER-APP/static/js/2.4087322a.chunk.js"
+  },
+  {
+    "revision": "b9ba9573485aaa709f25",
+    "url": "/KEEPER-APP/static/js/main.6538dc69.chunk.js"
+  },
+  {
+    "revision": "76b0635ab2ea37478534",
+    "url": "/KEEPER-APP/static/js/runtime-main.9fceca5c.js"
+  }
+]);
